68ea1aa167517b1d1a7205d145f3fe13e2646068
dhupka <dhupka1@gmail.com>
2021-02-15 00:40:05 -0500

    Clean

--- labs/lab2/lru.go
+++ labs/lab2/lru.go
@@ -1,3 +1,7 @@
+//Damian Hupka
+//ECGR-4090: Cloud Native Application Architecture Spring 20201
+//02/15/2021
+//Lab #2 - Interfaces (Implementing get() and put() of LRUCache)
 package lru
 
 import ( 
@@ -28,15 +32,15 @@ func (lru *lruCache) Get(key interface{}) (interface{}, error) {
 		lru.queue = append(lru.queue, node)
 		return lru.cache[key.(string)], nil					//Returning value of the key
 	}
-	return "-1", errors.New("Not Key")
+	return "-1", errors.New("Key does not exist.")
 
 }
 
 func (lru *lruCache) Put(key, val interface{}) error {
 	// Your code here....
 	//Check if key exists in cache, if so appends to queue (most recently used)
-	if node, found := lru.cache[key.(string)]; found {
-		lru.queue = append(lru.queue,node)
+	if _, found := lru.cache[key.(string)]; found {
+		lru.queue = append(lru.queue,lru.cache[key.(string)])
 		lru.cache[key.(string)] = val.(string)				//Updating cache value if key exists
 	} else {
 		if lru.size == lru.remaining { 						//Evicting least recently used if cache is full (from queue and cache), reducing size of cache
@@ -51,7 +55,7 @@ func (lru *lruCache) Put(key, val interface{}) error {
 	lru.cache[key.(string)] = val.(string)					//Updating cache value for non-existing key
 	return nil	
 	}
-	return errors.New("error")
+	return errors.New("ERROR.")
 }
 
 
